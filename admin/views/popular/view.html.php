96ba8a4d0c5470c5fad869e2a79059a495f0ecd1
Daniel Eliasson <daniel@stilero.com>
2013-12-25 16:54:51 +0100

    Fixed a bug with inclusion of MooTools

--- admin/views/popular/view.html.php
+++ admin/views/popular/view.html.php
@@ -2,7 +2,7 @@
 /**
  * Feed View
  *
- * @version  1.0
+ * @version  1.1
  * @author Daniel Eliasson - joomla at stilero.com
  * @copyright  (C) 2012-okt-21 Stilero Webdesign http://www.stilero.com
  * @category Components
@@ -32,7 +32,7 @@ class InstaboardViewPopular extends JViewLegacy{
         JHtml::stylesheet(JURI::root().'administrator/components/com_instaboard/assets/bootstrap/css/bootstrap-icon.min.css');
         JHtml::stylesheet(JURI::root().'administrator/components/com_instaboard/assets/bootstrap/css/bootstrap-thumbs.min.css');
         //JHtmlBehavior::framework(true); 
-        JHtml::_('behavior.mootools');
+        JHtml::_('behavior.framework'); 
         JHtml::script(JURI::root().'administrator/components/com_instaboard/assets/js/lazyload.js');
         JHTML::_('behavior.modal'); 
         $model = $this->getModel('popular');
